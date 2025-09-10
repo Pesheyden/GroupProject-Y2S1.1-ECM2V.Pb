ff7e36390b682dd7d3218e372be875c5c2ffdda2
Pesheyden <pasha.dan.dof@gmail.com>
2025-09-10 20:20:45 +0200

    Revert "Started working on the player controller"
    This reverts commit d080842c6c55fd92c64130ca5bed3a671448f589.

--- GroupProject-Y2S1.1-ECM2V.Pb/Assets/Features/PlayerMovement/Data_PlayerCameraController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/PlayerMovement/Data_PlayerCameraController", fileName = "PlayerCameraController_Data")]
-public class Data_PlayerCameraController : ScriptableObject
-{
-    public float SensitivityX;
-    public float SensitivityY;
-    public Vector2 YRotationClamp;
-}
